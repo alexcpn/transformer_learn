5cabc191fefbeb86d3511d8d9fdbf436f3f9eef0
Alex Punnen <alexcpn@gmail.com>
2023-03-11 05:51:16 +0530

    added for torch compile

--- gpt2_eval_model.py
+++ gpt2_eval_model.py
@@ -21,11 +21,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the model from the saved checkpoint directory
 checkpoint_dir ='./training/gpt2-epoch-50-7:54-4-6'
-checkpoint_dir ='./training/gpt2-epoch-50 2023-03-07 11:13:46.62'  #Loss: 0.06946726143360138
-#checkpoint_dir ='./training/gpt2-epoch-100' #Epoch 49 complete. Loss: 0.07775882631540298
-
+checkpoint_dir ='./training/gpt2-epoch-50 2023-03-07 11:13:46.62'  # # Unfreeze 6: Loss: 0.06946726143360138
+#checkpoint_dir ='./training/gpt2-epoch-200-2023-03-08 20:00:21.576489' # Unfreeze 4: Loss: 0.0677826926112175
+#checkpoint_dir ='./training/gpt2-epoch-1000-2023-03-08 22:19:56.069333'# tain all layers: Loss: 0.040
+checkpoint_dir ='./model/gpt2-epoch-5-2023-03-10 09:56:27.395044' #.74
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2',model_max_length=1024)
-tokenizer.pad_token = tokenizer.eos_token
+#tokenizer.pad_token = tokenizer.eos_token
 
 model = GPT2LMHeadModel.from_pretrained(checkpoint_dir,pad_token_id=tokenizer.eos_token_id)
 model.to(device)
@@ -44,13 +45,18 @@ while True:
             print("Exiting")
             break
       print(f'Processing Message from input()')
-      prompt = f"{question}"
-      encoding = tokenizer(prompt, return_tensors='pt').to(device)
+      prompt = f"{ question}"
+      #encoding = tokenizer(prompt, return_tensors='pt').to(device)
       # print(encoding['input_ids'])
       # print(encoding['attention_mask'])
-      outputs = model.generate(input_ids=encoding['input_ids'],
-                  attention_mask=encoding['attention_mask'],
-                  max_length=132, num_beams=4, early_stopping=True)
+      # outputs = model.generate(input_ids=encoding['input_ids'],
+      #             attention_mask=encoding['attention_mask'],
+      #             max_length=132, num_beams=4, early_stopping=True)
+      # making same as capability 
+      encoded_input = tokenizer(prompt, truncation=True, padding=False, return_tensors="pt")
+      outputs = model.generate(input_ids = encoded_input.input_ids.to(device),max_length=250,
+                              num_return_sequences=1)
+
       answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
       cprint(f"Question: {question}\n", 'green')
       cprint(f"Generated\n", 'cyan') 
@@ -60,49 +66,3 @@ while True:
       
 
 
-
-''' 
-If you shoot at night when 
-generated
---------------------
-If you shoot at night when  is dark the shutter speed becomes slow automatically
- shooting so that both the subject and background are properly exposed
-  If you do not want slow shutter speed to be set Flash sync speed in Av mode in
-   Custom Functions to or The flash pops up by itself In the and modes the flash will 
-   automatically when flash is necessary The flash does not fire If you shoot continuously 
-   with the flash at short intervals the flash might stop operating to protect the flash unit
-    can not set flash exposure compensation with External flash func setting If flash exposure
-     compensation has been set on an external Speedlite Flash exp comp can not be set in the]
-      External flash func setting screen Also if you set
-orginal
---------
-If you shoot at night when the background is dark the shutter speed
-becomes slow automatically shooting so that both the
-subject and background are properly exposed If you do not want
-slow shutter speed to be set set Flash sync speed in Av mode
-in Custom Functions to or
-The flash pops up by itself
-In the and modes the flash will
-automatically when flash is necessary
-The flash does not fire
-If you shoot continuously with the flash at short intervals the
-flash might stop operating to protect the flash unit
-can not set flash exposure compensation with External
-flash func setting
-If flash exposure compensation has been set on an external Speedlite
-Flash exp comp can not be set in the External flash func
----------------
-How to shoot at night?
-
-How to shoot at night? The Night Portrait mode blurs the background to make the human subject stand out It also makes skin tones and the hair look softer than with the Full Auto mode Shooting Tips The further the distance between the subject and background the better The further the distance between the subject and background the more blurred the background will look The subject will also stand out better in front of plain dark background Use telephoto lens If you have zoom lens use the telephoto end to fill the frame with the subject from the waist up Move in closer if necessary Focus the face Check that the AF point covering the face flashes in red If you hold down the shutter button you
-ale
-
-with 50 iteration and clean2
-
-How to shoot at night? The Night Portrait mode blurs the background to make the human subject stand out It also makes skin tones and the hair look softer than with the Full Auto mode Shooting Tips The further the distance between the subject and background the better The further the distance between the subject and background the more blurred the background will look The subject will also stand out better in front of plain dark background Use telephoto lens If you have zoom lens use the telephoto end to fill the frame with the subject from the waist up Move in closer if necessary Focus the face Check that the AF point covering the face flashes in red If you hold down the shutter button you
-
-with 100 iterations and clean2
-
-How to shoot at night? When the background is dark the shutter speed becomes slow automatically shooting so that both the subject and background are properly exposed If you do not want slow shutter speed to be set set Flash sync speed in Av mode in Custom Functions to or The flash pops up by itself In the and modes the flash will automatically when flash is necessary The flash does not fire If you shoot continuously with the flash at short intervals the flash might stop operating to protect the flash unit can not set flash exposure compensation with External flash func setting If flash exposure compensation has been set on an external Speedlite Flash exp comp can not be set in the External flash func setting screen Also
-
-'''
--- gpt2_train_model.py
+++ gpt2_train_model.py
@@ -6,8 +6,16 @@ from utils import get_batch
 from transformers import  get_linear_schedule_with_warmup # for training
 import logging as log
 from datetime import datetime
+import re
+import torch._dynamo.config
 
-outfile = "./training/training_" + str(datetime.now()) +".log"
+print(torch.__version__)
+#torch.set_float32_matmul_precision('high')
+torch._dynamo.config.log_level = log.DEBUG
+torch._dynamo.config.verbose = True
+
+time_hash=str(datetime.now())
+outfile = "./training/training_" +time_hash +".log"
 log.basicConfig(
     level=log.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -18,6 +26,7 @@ log.basicConfig(
 )
 
 model_name = 'gpt2'
+
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 #log.info(TokenizerDetails(tokenizer) # model_max_length: 1024 # vocab_size: 50257
@@ -46,19 +55,17 @@ attention_mask=encoding.attention_mask.view(-1)
 #  model (23552 > 1024). Running this sequence through the model will result in indexing errors
 # However we are not running through the model; We will add it to an array and train with block_size
 
-block_size = int(tokenizer.model_max_length/4) # tokenizer.model_max_length=1024
-
 # Load the T5 model 
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
-# Freeze bottom 6 layers
-#https://towardsdatascience.com/conditional-text-generation-by-fine-tuning-gpt-2-11c1a9fc639d
-for parameter in model.parameters():
-    parameter.requires_grad = False
+# # Freeze bottom 8 layers
+# #https://towardsdatascience.com/conditional-text-generation-by-fine-tuning-gpt-2-11c1a9fc639d
+# for parameter in model.parameters():
+#     parameter.requires_grad = False
 
 for i, m in enumerate(model.transformer.h):        
     #Only un-freeze the last n transformer blocks
-    if i >= 6:
+    if i >= 10:
         for parameter in m.parameters():
             parameter.requires_grad = True 
 
@@ -70,21 +77,30 @@ for parameter in model.lm_head.parameters():
 
 # Fine-tune the model on the training data
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# We could optimize training using Pytorch 2.0
+# It neds cuda 11.6+ ,  it crashes as of now
+if int(re.search(r'\d+', torch.__version__).group()) >= 2:
+    # for pytorch 2.0
+    model =torch.compile(model)
+    log.info(f"Compiled the model for speed up")
+
 model.to(device)
 # learning_rate = 6e-4 # ??
-#optimizer = torch.optim.Adam(model.parameters(), lr=3e-5) - use get_linear_schedule_with_warmup
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-5) 
 
 # use the data from ChatGPT - need to recheck this
 # Set up the training parameters
 train_batch_size = 4
+block_size = int(tokenizer.model_max_length/8) # tokenizer.model_max_length=1024
 num_train_epochs = 50
 num_warmup_steps = 100
 max_grad_norm = 1.0
 
 # Set the optimizer and learning rate scheduler
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+#optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
 num_train_steps = len_train_data // train_batch_size * num_train_epochs
-lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_train_steps)
+#lr_scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_train_steps)
 
 model.train()
 for epoch in range(num_train_epochs):
@@ -94,26 +110,24 @@ for epoch in range(num_train_epochs):
         # do the batch size manipulation here
         x,y= get_batch(len_train_data,input_ids,attention_mask,device,block_size=block_size,batch_size=train_batch_size)
         #x.shape=torch.Size([batch_size, blocksize])
-        
         # attention_mask given by tokenize is array of ones= [1,1,..], that is attend to all tokens
         # if we do not give the parameter, the model will attend to all tokens by default
         # Regarding labels - we can use T5 span masked modelling
         # https://github.com/huggingface/transformers/blob/main/examples/flax/language-modeling/run_t5_mlm_flax.py
         #labels = input_ids[:, 1:].clone()  # shift labels to the right #todo
         outputs = model(input_ids=x,attention_mask=y,labels=x)
-        
         loss = outputs.loss
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        #torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
         optimizer.step()
-        lr_scheduler.step()
+        #lr_scheduler.step()
         optimizer.zero_grad()
     log.info(f"Epoch {epoch} complete. Loss: {loss.item()}")
     # Save the model checkpoint every 10th
-    checkpoint_dir = f"./training/{model_name}-epoch-{epoch+1}"
+    checkpoint_dir = f"./model/{model_name}-epoch-{epoch+1}-{time_hash}"
     model.save_pretrained(checkpoint_dir)
     # delete the previous save epoch
-    checkpoint_dir = f"./training/{model_name}-epoch-{epoch}"
+    checkpoint_dir = f"./model/{model_name}-epoch-{epoch}-{time_hash}"
     try:
         shutil.rmtree(checkpoint_dir)
     except:
