a0476d4ed34e91ada25a0ef618b1097e0e0a30a1
Alex Punnen <alexcpn@gmail.com>
2023-04-19 16:32:51 +0530

    made eval better

--- gpt2_eval_model.py
+++ gpt2_eval_model.py
@@ -20,7 +20,7 @@ log.basicConfig(
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the model from the saved checkpoint directory
-checkpoint_dir ='./test-gpt2-4/gpt2-epoch-50-2023-04-18 20:32:35.343858'
+checkpoint_dir ='./small3-gpt2-5/gpt2-epoch-100-2023-04-19 13:57:47.849368'
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2',model_max_length=1024,padding_side='left')
 tokenizer.add_special_tokens({'pad_token': '[PAD]'}) # note we have used this while training
 model = GPT2LMHeadModel.from_pretrained(checkpoint_dir,pad_token_id=50257)
@@ -53,31 +53,29 @@ while True:
       print(f'Processing Message from input()')
       prompt = f"{question}"
       encoded_input = tokenizer(prompt,truncation=True,padding=True, return_tensors='pt').to(device)
-      outputs = model.generate(input_ids = encoded_input.input_ids.to(device),
-                        attention_mask = encoded_input.attention_mask.to(device),
-                                 min_new_tokens=200,
-                                 max_new_tokens=250,
-                                 #num_beams=1,# 1 means no beam search
-                                 #early_stopping=True,
-                                 #num_beam_groups=1, #1 default
-                                 #temperature=1, # 1 default
-                                 repetition_penalty=1.5,
-                                 no_repeat_ngram_size=1)
+    #   outputs = model.generate(input_ids = encoded_input.input_ids.to(device),
+    #                     attention_mask = encoded_input.attention_mask.to(device),
+    #                              #min_new_tokens=200,
+    #                              #max_new_tokens=250,
+    #                              #num_beams=1,# 1 means no beam search
+    #                              #early_stopping=True,
+    #                              #num_beam_groups=1, #1 default
+    #                              #temperature=1, # 1 default
+    #                              max_length=250,
+    #                              repetition_penalty=1.5,
+    #                              no_repeat_ngram_size=2,
+    #                              early_stopping=True)
 
-      answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-      cprint(f"Question: {question}\n", 'green')
-      cprint(f"Generated\n", 'cyan') 
-      cprint(f"{answer}\n", 'blue') 
       log.info(f"Question: {question}")
-      log.info(f"Generated: {answer}") 
       # the below is better 
       test_output = model.generate(input_ids=encoded_input.input_ids.to(device), 
                     attention_mask = encoded_input.attention_mask.to(device),
                     max_length=250,
                     num_return_sequences=1)
       test_answer = tokenizer.decode(test_output[0], skip_special_tokens=True)
-      log.info(f"Generated 2: {test_answer}")
-      cprint(f"Generated 2\n", 'cyan') 
+      log.info(f"Generated : {test_answer}")
+      cprint(f"Question: {question}\n", 'green')
+      cprint(f"Generated \n", 'cyan') 
       cprint(f"{test_answer}\n", 'blue') 
 
       
