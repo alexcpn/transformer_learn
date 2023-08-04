ea46fec3092a8da85a94d72948387a84ecf084b4
Alex Punnen <alexcpn@gmail.com>
2023-08-04 17:18:58 +0530

    minor change

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from llama2_7b_4bit import LLAMa2_7b_4bitQ
-
+    import traceback
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -18,13 +18,14 @@ def index():
 def handle_message(data):
     print('received query: ' + data['input'])
     print('received system_message: ' + data['system_message'])
-    prompt_template = llam2.create_prompt(data['input'],data['system_message'])
+    prompt_template = llam2.create_prompt(data['input'].strip(),data['system_message'].strip())
     try:
         output = llam2.generate_ouputput(prompt_template)
         response =llam2.get_chat_response(output)
         data = data['input'].replace('\n', '<br>')  # replace newline characters with <br> tags
         emit('response', {'query': data,'response': 'Response: '+ response}, broadcast=True)
-    except:
+    except Exception :
+        print(traceback.format_exc())
         emit('response', {'query': data,'response': 'Sorry got an Error From Server: '}, broadcast=True)
 
 if __name__ == '__main__':
