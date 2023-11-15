c6884119a00e9fcd9dbed27d645a00cf7b98e1e5
acp <alex.punnen@nokia.com>
2023-11-15 11:59:23 +0530

    Mistral quantised 4 bit training and loading

--- mistral/eval_ft_mistral_lora.py
+++ mistral/eval_ft_mistral_lora.py
@@ -3,6 +3,7 @@ import torch
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    BitsAndBytesConfig,
 )
 from peft import  PeftModel
 from datetime import datetime
@@ -35,17 +36,51 @@ tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
 
 
 device_map = {"": 0}
+
+# Loading from a saved state_dict, 
+#  nokiaop@namsrv01:~/alex$ python eval_ft_mistral_lora.py mistral-direct/epoch-99-2023-11-14\ 05\:51\:34.180042
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type= "nf4",
+    bnb_4bit_compute_dtype="float16",
+    bnb_4bit_use_double_quant=False,
+)
+
+# Load base model
 model = AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.bfloat16, load_in_4bit=False, device_map=device_map)
+    model_name,
+    torch_dtype=torch.bfloat16, 
+    quantization_config=bnb_config,
+    device_map=device_map
+)
 
-#Loading the QLora trained model
+state_dict = torch.load(trained_model_adapter)
+model.load_state_dict(state_dict)
 
-model_to_merge = PeftModel.from_pretrained(model, trained_model_adapter) #14 GB GPU T4
-model = model_to_merge.merge_and_unload()
+
+# Uncomment below for Loading the QLora trained model
+#model = AutoModelForCausalLM.from_pretrained(
+#            model_name, torch_dtype=torch.bfloat16, load_in_4bit=False, device_map=device_map)
+# model_to_merge = PeftModel.from_pretrained(model, trained_model_adapter) #14 GB GPU T4
+# model = model_to_merge.merge_and_unload()
+
 
 model.eval()
 
-outfilename = 'fmistral_out' + formatted_time +'.txt'
+# prompt =f""" How do I start with Network As Code to increase QoS?  """
+# prompt_template=f'''<s>[INST]
+# {prompt}[/INST]]'''
+
+# inputs = tokenizer(prompt_template, return_tensors="pt")
+# outputs = model.generate(**inputs.to('cuda') ,max_new_tokens=2000)
+# output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+# #print(output)
+# print("-"*80)
+# outputcont = "".join(output)
+# parts = outputcont.split("[/INST]", 1)
+# print(parts[1])
+
+outfilename = 'epoch5_mistral_out' + formatted_time +'.txt'
 while True:
     lines = []
     print("Enter your text (Type 'E' on a new line to Enter or exit):")
@@ -78,7 +113,7 @@ while True:
         print(prompt_template)
         
     inputs = tokenizer(prompt_template, return_tensors="pt")
-    outputs = model.generate(**inputs.to('cuda') ,repetition_penalty=1.2,max_new_tokens=500)
+    outputs = model.generate(**inputs.to('cuda') ,repetition_penalty=1.2,max_new_tokens=1000)
     output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
     #print(output)
     print("-"*80)
@@ -86,4 +121,6 @@ while True:
     parts = outputcont.split("[/INST]", 1)
     print(parts[1])
     with open(outfilename, "a", encoding='utf-8') as myfile:
+        myfile.write(prompt_template)
         myfile.write(parts[1])
+        myfile.write("-"*80)
